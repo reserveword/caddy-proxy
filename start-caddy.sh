c0d7a4bb7b1b29d1c1a394b4567c0aeda6a7d235
reserveword <reserveword@hit.edu.cn>
2020-10-25 23:34:46 +0800

    add acme.sh things to script

--- start-caddy.sh
+++ start-caddy.sh
@@ -3,12 +3,20 @@
 if [ -n "$VIRTUAL_HOST" ]; then
   echo $VIRTUAL_HOST > /etc/Caddyfile
 elif [ -n "$VIRTUAL_HOSTS" ]; then
+  VIRTUAL_HOST=${VIRTUAL_HOSTS}$'\n'${AppName}.herokuapp.com
   echo $VIRTUAL_HOSTS > /etc/Caddyfile
 else
-  echo ERROR: no VIRTUAL_HOSTS or VIRTUAL_HOST env var provided >>/dev/stderr
-  exit 1
+  #echo ERROR: no VIRTUAL_HOSTS or VIRTUAL_HOST env var provided >>/dev/stderr
+  VIRTUAL_HOST=${AppName}.herokuapp.com
 fi
 
 cat /etc/Caddyfile.tpl >> /etc/Caddyfile
+
+if [ -n "$OTHER_DNS_API_ENV" ]; then
+  $(echo "export $a")
+fi
+if [ -n "$USE_ACME_SH" ]; then
+  acme.sh --issue --dns $USE_ACME_SH $(echo $VIRTUAL_HOST | awk '{print "-b " $0}')
+fi
 
 exec /usr/bin/caddy -quic -agree --conf /etc/Caddyfile --log stdout $@
